a5298acb53f46f1c61e96bf8af01029780b30933
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-07-16 03:03:39 +0530

    prettier

--- src/app/api/token/route.ts
+++ src/app/api/token/route.ts
@@ -1,15 +1,15 @@
-export const dynamic = "force-dynamic";
+export const dynamic = 'force-dynamic'
 
-export const fetchCache = "force-no-store";
+export const fetchCache = 'force-no-store'
 
-import client from "@/lib/session.server";
-import { NextResponse } from "next/server";
+import client from '@/lib/session.server'
+import { NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
-  const searchParams = new URL(request.url).searchParams;
-  const name = searchParams.get("name");
-  if (!name) return new Response(null, { status: 400 });
-  if (!client) return new Response(null, { status: 500 });
-  const userToken = client.createUserToken(name);
-  return NextResponse.json({ userToken });
+  const searchParams = new URL(request.url).searchParams
+  const name = searchParams.get('name')
+  if (!name) return new Response(null, { status: 400 })
+  if (!client) return new Response(null, { status: 500 })
+  const userToken = client.createUserToken(name)
+  return NextResponse.json({ userToken })
 }
--- src/lib/session.server.ts
+++ src/lib/session.server.ts
@@ -1,9 +1,9 @@
-import { connect, type StreamClient } from "getstream";
+import { connect, type StreamClient } from 'getstream'
 
-let client: StreamClient | null = null;
+let client: StreamClient | null = null
 
 if (process.env.STREAM_API_KEY && process.env.STREAM_API_SECRET && !client) {
-  client = connect(process.env.STREAM_API_KEY, process.env.STREAM_API_SECRET);
+  client = connect(process.env.STREAM_API_KEY, process.env.STREAM_API_SECRET)
 }
 
-export default client;
+export default client
