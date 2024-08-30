1a42d12d56cb24638815785fe03350f2b8110d3b
Rishi Raj Jain <jain71000@gmail.com>
2024-08-30 10:45:41 +0530

    further simplify

--- src/app/api/token/route.ts
+++ src/app/api/token/route.ts
@@ -2,20 +2,18 @@ export const dynamic = 'force-dynamic'
 
 export const fetchCache = 'force-no-store'
 
-import { connect, type StreamClient } from 'getstream'
+import { connect } from 'getstream'
 import { NextResponse } from 'next/server'
 
-let client: StreamClient | null = null
+if (!process.env.NEXT_PUBLIC_STREAM_API_KEY) throw new Error(`NEXT_PUBLIC_STREAM_API_KEY environment variable is not defined.`)
+if (!process.env.STREAM_API_SECRET) throw new Error(`STREAM_API_SECRET environment variable is not defined.`)
 
-if (process.env.STREAM_API_KEY && process.env.STREAM_API_SECRET && !client) {
-  client = connect(process.env.STREAM_API_KEY, process.env.STREAM_API_SECRET)
-}
+const client = connect(process.env.NEXT_PUBLIC_STREAM_API_KEY, process.env.STREAM_API_SECRET)
 
 export async function GET(request: Request) {
   const searchParams = new URL(request.url).searchParams
   const name = searchParams.get('name')
   if (!name) return new Response(null, { status: 400 })
-  if (!client) return new Response(null, { status: 500 })
   const userToken = client.createUserToken(name)
   return NextResponse.json({ userToken })
 }
